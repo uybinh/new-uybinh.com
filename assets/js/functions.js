d6edee3d8001752850fee1009e61a2bec0f01e77
Uy Binh <uybinh@gmail.com>
2018-10-07 20:00:34 +0700

    Add script to toggle active link

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Get started!
+  activeLink();
 });
+
+function activeLink() {
+  const pathName = window.location.pathname.replace(/\//g, "");
+  const links = document.querySelectorAll("a");
+  links.forEach(link => {
+    const path = link.href.split("/").pop();
+    path === pathName && link.classList.toggle("selected");
+  });
+}
